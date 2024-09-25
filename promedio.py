43e0b547d12dd773d6ea44d745206a6067eebd78
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-24 19:47:27 -0600

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,12 @@
+# Solicitar tres números al usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+# Calcular el promedio
+promedio = (num1 + num2 + num3) / 3
+# Mostrar el promedio
+print(f"El promedio de los tres números es: {promedio:.2f}")
+
+print("")
+print("Mejia Suarez Emmanuel Alexander:mi practica es un programa que solicite al usuario ingresar tres números para luego mostrarle el promedio de los tres")
+print("")
